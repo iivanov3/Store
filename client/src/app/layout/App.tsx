0b078b3dfa09b4ee66801fd7c94de83772ed18db
Ivan <94326614+iivanov3@users.noreply.github.com>
2024-07-15 17:51:14 +0200

    added product cards

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import Header from './Header';
+import { useState } from 'react';
+import Catalog from '../../features/Catalog';
+
+function App() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const palleteType = darkMode ? 'dark' : 'light';
+
+    const theme = createTheme({
+        palette: {
+            mode: palleteType,
+            background: {
+                default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+            }
+        }
+    })
+
+    function handleThemeChange() {
+        setDarkMode(!darkMode);
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+            <Container>
+                <Catalog />
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default App
---
+++ client/src/features/Catalog.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import ProductList from './ProductList';
+import { Product } from '../app/models/product';
+
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data));
+    }, [])
+
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
+}
