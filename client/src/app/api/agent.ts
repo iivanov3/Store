3ebfb04fa1243dec3b9387875748a39de46e4c18
Ivan <94326614+iivanov3@users.noreply.github.com>
2025-04-20 11:55:54 +0200

    login and register

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from '../router/Routes';
-import { fetchFilters } from "../../features/catalog/catalogSlice";
 import { PaginatedResponse } from "../models/pagination";
+import { store } from "../store/configureStore";
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500))
 
@@ -10,6 +10,12 @@ axios.defaults.baseURL = 'http://localhost:5000/api/';
 axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().account.user?.token;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
     await sleep();
@@ -35,7 +41,7 @@ axios.interceptors.response.use(async response => {
             toast.error(data.title);
             break;
         case 401:
-            toast.error(data.title);
+            toast.error(data.title || 'Unauthorized');
             break;
         case 500:
             router.navigate('/server-error', { state: { error: data } })
@@ -73,10 +79,17 @@ const Basket = {
     removeItem: (productId: number, quantity = 1) => requests.del(`basket?productId=${productId}&quantity=${quantity}`)
 }
 
+const Account = {
+    login: (values: any) => requests.post('account/login', values),
+    register: (values: any) => requests.post('account/register', values),
+    currentUser: () => requests.get('account/currentUser'),
+}
+
 const agent = {
     Catalog,
     TestErrors,
-    Basket
+    Basket,
+    Account
 }
 
 export default agent;
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
     darkMode: boolean;
@@ -33,6 +34,7 @@ const navStyles = {
 
 export default function Header({ handleThemeChange, darkMode }: Props) {
     const { basket } = useAppSelector(state => state.basket);
+    const { user } = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
@@ -64,23 +66,28 @@ export default function Header({ handleThemeChange, darkMode }: Props) {
                 </List>
 
                 <Box display='flex' alignItems='center'>
-                    <IconButton component={Link} to='/basket' size='large' sx={navStyles}>
+                    <IconButton component={Link} to='/basket' size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
                         <Badge badgeContent={itemCount} color='secondary'>
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
-                    <List sx={{ display: 'flex' }}>
-                        {rightLinks.map(({ title, path }) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? (
+                        <SignedInMenu />
+                    ) :
+                        (
+                            <List sx={{ display: 'flex' }}>
+                                {rightLinks.map(({ title, path }) => (
+                                    <ListItem
+                                        component={NavLink}
+                                        to={path}
+                                        key={path}
+                                        sx={navStyles}
+                                    >
+                                        {title.toUpperCase()}
+                                    </ListItem>
+                                ))}
+                            </List>
+                        )}
                 </Box>
 
             </Toolbar>
